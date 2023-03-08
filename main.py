515826f8383576af8b4a8c41bdd0c6accf59a8ea
vskamaev <vskamaev@miem.hse.ru>
2023-03-08 20:04:12 +0300

    Progress in embedding

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ parser.add_argument('-U', action='store_true', help='Extract watermark (Uncover 
 def main():
     args = parser.parse_args()
 
-    img = Image.open(args.image)
-    new_shape = GetNewImageSize(img.size)
-    img = img.crop((0, 0, new_shape[0], new_shape[1]))
+    # img = Image.open(args.image)
+    # new_shape = GetNewImageSize(img.size)
+    # img = img.crop((0, 0, new_shape[0], new_shape[1]))
+    #
+    # layers = np.array(img)
+    # img.close()
+    #
+    # for color, layer in enumerate(layers):
+    #     ci_blocks = SplitArrayBySquareBlocks(layer, 4)
+    #     for ci_block in ci_blocks:
+    #         subsample_blocks = Create_Subsample_Blocks(ci_block)
+    #         interpolated_blocks = [Interpolate_Subsample_Block(ssb) for ssb in subsample_blocks]
 
-    layers = (np.array(col) for col in img.split())
-    img.close()
+    ci_block = np.array([[15, 18, 19, 14], [13, 15, 16, 17], [18, 17, 14, 13], [23, 24, 25, 22]])
+    subsample_blocks = Create_Subsample_Blocks(ci_block)
+    interpolated_blocks = [Interpolate_Subsample_Block(ssb) for ssb in subsample_blocks]
+    # print(*subsample_blocks, sep='\n\n')
+    # print(*interpolated_blocks, sep='\n\n')
 
-    for color, layer in enumerate(layers):
-        ci_blocks = SplitArrayBySquareBlocks(layer, 4)
-
-
-    a = 1
+    #print(Lagrange_Interpolation('01000111010', '11111111111111'))
 
 
 if __name__ == '__main__':
--- src/functions.py
+++ src/functions.py
@@ -9,12 +9,46 @@ def SplitArrayBySquareBlocks(arr: np.ndarray, block_side: int) -> np.ndarray:
     return blocks.reshape(arr_h * arr_w // (block_side * block_side), block_side, block_side)
 
 
-def GetNewImageSize(shape: tuple) -> tuple[int, int]:
-    h, w = shape
-    return h - h % 4, w - w % 4
+def TDC(block: np.ndarray) -> str:
+    center = min(block[1][1], block[1][2], block[2][1], block[2][2])
+    edges = np.array(
+        [block[3][3], block[3][2], block[3][1], block[3][0], block[2][0], block[1][0], block[0][0], block[0][1],
+         block[0][2], block[0][3], block[1][3], block[2][3]])
+    lbp = bin(np.bitwise_xor.reduce(edges[edges >= center]))[2:].zfill(8)
+    tdc1 = int(lbp[1], 2) ^ int(lbp[3], 2) ^ int(lbp[5], 2) ^ int(lbp[7], 2)
+    tdc2 = int(lbp[0], 2) ^ int(lbp[2], 2) ^ int(lbp[4], 2) ^ int(lbp[6], 2)
+    return str(tdc1) + str(tdc2)
 
 
-def NearestCorner(i: int) -> int:
+def Lagrange_Interpolation(M: str, mu: str) -> np.ndarray:  # Tested && CORRECT
+    a = int(mu[:6], 2)
+    b = int(mu[6:], 2)
+    gamma = int(M, 2) ^ int(mu[:11], 2)
+    Ds = []
+    for Y in range(1, 5):
+        f = a * Y * Y + b * Y + gamma
+        D_bin = (bin(f)[2:]).zfill(12)
+        print(int(D_bin, 2))
+        D = [int(D_bin[i * 2: i * 2 + 2], 2) for i in range(6)]
+        Ds.append(D)
+    return np.array(Ds)
+
+
+def Create_Watermark_Images_From_Interpolated_Images(block: np.ndarray, D: np.ndarray) -> np.ndarray:
+    new_block = block.copy()
+    for v in range(6):
+        val = (block[1][2] + 2 * block[2][1] + 3 * block[2][2]) % 4
+        if 0 <= v <= 1:
+            i, j = 0, v + 1
+        elif 2 <= v <= 3:
+            i, j = v - 1, 3
+        else:
+            i, j = 3, 6 - v
+        diff = D[v - 1] - val
+        new_block[i][j] = block[i][j] - block[i][j] % 4 + diff
+
+
+def NearestCorner(i: int) -> int:  # Tested && CORRECT
     if i == 0 or i == 7:
         return 0
     if i == 1 or i == 2:
